ea2668fa584716fccb5cc164b636cfeeb0408c7a
Amber <woll.ambermadison@gmail.com>
2023-12-14 22:01:46 -0700

    Revert "link update"
    This reverts commit 4688862517c63a62702a2f76de6a803e3c4b54f8.

--- app.jsx
+++ app.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
-import { Login } from './login.jsx';
-import { Play } from './play.jsx';
-import { Leaderboard } from './leaderboard.jsx';
-import { Help } from './help.jsx';
+import { Login } from './login/login';
+import { Play } from './play/play';
+import { Scores } from './scores/scores';
+import { About } from './about/about';
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
@@ -18,23 +18,16 @@ export default function App() {
 
                     <nav>
                         <menu>
-                            <li><NavLink className='nav-link' to='index'>Home</NavLink></li>
-                            <li><NavLink className='nav-link' to='login'>Login</NavLink></li>
-                            <li><NavLink className='nav-link' to='play'>Play the Game</NavLink></li>
-                            <li><NavLink className='nav-link' to='leaderboard'>Leaderboard</NavLink></li>
-                            <li><NavLink className='nav-link' to='help'>Help</NavLink></li>
+                            <li><a href="index.html">Home</a></li>
+                            <li><a href="login.html">Login</a></li>
+                            <li><a href="play.html">The Game</a></li>
+                            <li><a href="leaderboard.html">Leaderboard</a></li>
+                            <li><a href="help.html">Help</a></li>
                         </menu>
                     </nav>
                 </header>
         
-                <Routes>
-                    <Route path='/index' element={<Home />} exact />
-                    <Route path='/login' element={<Login />} exact />
-                    <Route path='/play' element={<Play />} />
-                    <Route path='/leaderboard' element={<Leaderboard />} />
-                    <Route path='/help' element={<Help />} />
-                    <Route path='*' element={<NotFound />} />
-                </Routes>
+                <main>App components go here</main>
         
                 <footer>
                     <hr />
@@ -45,10 +38,4 @@ export default function App() {
             </div>
         </BrowserRouter>
     );
-
   }
-
-  function NotFound() {
-    return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
-  }
-  
